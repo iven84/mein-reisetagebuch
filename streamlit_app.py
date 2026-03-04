c4a664a0cb3c65ed7141b4111ad78703836fbb71
iven84 <ivendittmann@gmx.net>
2026-03-04 11:36:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ st.set_page_config(page_title="Reisetagebuch", page_icon="🌍")
 
 # 2. DEIN LINK - Absolut sauber ohne Leerzeichen!
 # Falls dein Browser "Import" oder "aus" schreibt -> bitte manuell korrigieren!
-URL = "https://docs.google.com/spreadsheets/d/1aIMSYHxw89-d-FIqsxq9FQJLrrROVdmYceABxglZmq8/edit?usp=sharing"
+URL = "https://docs.google.com/spreadsheets/d/1aIMSYHxW89-d-FIQsxq9FQJLrrROVdmYceABxglZmq8/edit?usp=drivesdk"
 MEIN_TABELLEN_LINK = URL.strip()
 BLATT_NAME = "Tabelle1"
 
